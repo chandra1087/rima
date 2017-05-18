f2d9900442b0409603288a7854ab769435da5397
chandra1087 <chandra989kirana@gmail.com>
2017-05-18 11:41:36 +0800

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
 return {
 
 	-- Your authorization token from the botfather.
-	bot_api_key = '',
+	bot_api_key = '399774682:AAEHKXi3YyRI_-hi7qeQr5xtn2D9Qd7P-2o',
 	-- Your Telegram ID.
-	admin = ,
+	admin = ,141960232
 	-- about the bot
 	text = [[
 	<text hear>
 	]],
         -- Your authorization token Api from the http://developer.simsimi.com/
-	simsimi_key = 'a7ac3b74-3d81-444b-a344-dd8024132bce',
+	simsimi_key = '67b0f1a7-685c-44e5-85ba-d1a597feb9e5',
 	-- lang 45 
 	lang = 'ar','en','af','bg','ca','ch','cs','cy','da','de','el','es','eu','fi','fr','he','hi','hr','hu','id','it','ja','kh','ko','lt','ml','ms','nb','nl','pa','ph','pl','pt','ro','rs','ru','sk','sv','ta','te','th','tr','uk','vn','zh',
 	-- true or false
